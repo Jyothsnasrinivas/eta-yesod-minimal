b0de4aeeb6368dddd836ea60dfd05e3a962fbc58
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2018-08-16 16:29:20 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center">Yesod-minimal example in Eta</h1>
+
+## Running the server
+
+1) Fire up the terminal and enter the commands:
+
+  ```
+  $ git clone https://github.com/Jyothsnasrinivas/eta-yesod-minimal.git
+  $ cd eta-yesod-minimal
+  $ etlas run
+  ```
+
+2) Open the url `http://localhost:3000` in your favourite browser.
---
+++ src/Home.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes, ScopedTypeVariables       #-}
+module Home where
+
+import Foundation
+import Yesod.Core
+import Data.Monoid (mconcat)
+import Data.FileEmbed
+import Data.Text.Lazy (Text)
+import Data.Text.Lazy.Encoding
+import Text.Hamlet
+import Text.Blaze.Html.Renderer.Text
+
+getHomeR :: Handler Html
+getHomeR = do return [shamlet|
+               <!DOCTYPE html>
+               <html>
+                 <head>
+                   <title>Eta is running on Yesod!
+                 <body style="background-color: #2cd4d9">
+                   <div class="box">
+                     <h1 style="color: #ffffff; text-align: center;">Yesod example in Eta!
+                     <h3 style="color: #ffffff; text-align: center;">
+                     <p style="text-align:center">
+                         <a style="color: white" href="#">HTML
+                     <p style="text-align:center">
+                         <a style="color: white" href="#">JSON
+                 <style>
+                   .box{
+                        width: 400px;
+                        background: #43414e;
+                        border-radius: 10px;
+                        padding: 60px;
+                        margin: auto;
+                        position: relative;
+                        top: 250px;
+                        -webkit-box-shadow: 0 10px 6px -6px #777;
+                        -moz-box-shadow: 0 10px 6px -6px #777;
+                        box-shadow: 0 10px 6px -6px #777;
+                       }
+|]
